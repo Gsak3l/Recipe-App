112e98394dd6e361e8cfc2b75eb944120a8f4f4e
gsak3l <gsakel5@gmail.com>
2019-10-02 20:04:22 +0300

    api works

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
+import { async } from 'q';
 
 const App = () => {
   //api stuff
   const APP_ID = "3c06f5ed";
   const APP_KEY = "fe4edfd4233c805f05316543caaba326";
-  const exampleRequest = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
-  const [counter, setCounter] = useState(0);
 
-  useEffect(() => {
-    console.log('i am here');
-  })
+  useEffect( () => {
+    getRecipes();
+  }, []); //by adding an empty set of arrays, my page doesn't render every single
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json(); //this will format it, so we can work with the data much easier
+    console.log(data);
+  }
 
   return (
     <div className="App">
       <form className="search-form">
         <input className="search-bar" type="text" />
-        <button className="search-button" onClick={()=> setCounter(counter + 1)} type="submit">{counter}</button>
+        <button className="search-button" type="submit">search </button>
       </form>
     </div>
   );
