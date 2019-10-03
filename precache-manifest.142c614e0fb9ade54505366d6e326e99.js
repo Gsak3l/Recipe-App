abdeb210070229a7cb393230944d4b671f533172
gsak3l <gsakel5@gmail.com>
2019-10-03 15:13:55 +0300

    Updates

---
+++ precache-manifest.142c614e0fb9ade54505366d6e326e99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99da8642115e25aab7cf0e852ac54deb",
+    "url": "/Recipe-App/index.html"
+  },
+  {
+    "revision": "ba3a8dba1e92b418bd1c",
+    "url": "/Recipe-App/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "66ba21596b36a5e83cb7",
+    "url": "/Recipe-App/static/css/main.4d452cea.chunk.css"
+  },
+  {
+    "revision": "ba3a8dba1e92b418bd1c",
+    "url": "/Recipe-App/static/js/2.83651f23.chunk.js"
+  },
+  {
+    "revision": "66ba21596b36a5e83cb7",
+    "url": "/Recipe-App/static/js/main.24962cef.chunk.js"
+  },
+  {
+    "revision": "8b04587e9de908ad05e5",
+    "url": "/Recipe-App/static/js/runtime-main.578bd87f.js"
+  }
+]);
