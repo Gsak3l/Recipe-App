a2e8c9f25dd67369d379670db0da746e8e96ff7b
gsak3l <gsakel5@gmail.com>
2019-10-03 11:50:33 +0300

    api works, recipes load correctly

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import { async } from 'q';
+import {async} from 'q';
+import Recipe from "./Recipe";
 
 const App = () => {
   //api stuff
   const APP_ID = "3c06f5ed";
   const APP_KEY = "fe4edfd4233c805f05316543caaba326";
+
+  const [recipes, setRecipes] = useState([]);
+
+  const [search, setSearch] = useState('');
 
 
   useEffect( () => {
@@ -15,15 +20,20 @@ const App = () => {
   const getRecipes = async () => {
     const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json(); //this will format it, so we can work with the data much easier
-    console.log(data);
+    setRecipes(data.hits);
   }
 
   return (
     <div className="App">
       <form className="search-form">
-        <input className="search-bar" type="text" />
+        <input className="search-bar" type="text" value={search}/>
         <button className="search-button" type="submit">search </button>
       </form>
+      {recipes.map(recipe => (<Recipe 
+        title={recipe.recipe.label} 
+        calories={recipe.recipe.calories}
+        image={recipe.recipe.image}
+        key={recipe.recipe.label}/>))}
     </div>
   );
 }
